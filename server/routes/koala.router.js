9c8f4531ab88d1afab0a4096c10de4def0218ff9
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-07-27 15:25:38 -0500

    database filled and linked

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,20 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
-
+const pool = require('../modules/pool');
 
 // GET
-
+koalaRouter.get('/', (req, res) =>{
+    let queryText = `SELECT * FROM "koalas";`
+    pool.query(queryText)
+        .then((result) =>{
+            res.send(result.rows)
+        })
+        .catch((error) => {
+            console.log("Error getting koalas", error)
+            res.sendStatus(500)
+        })
+});
 
 // POST
 
