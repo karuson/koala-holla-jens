d6ec170851023bdf22134f3ff934c6b2ff494a85
Ben Lynch <bdlynch7@gmail.com>
2023-07-28 11:00:54 -0500

    merge

--- database.sql
+++ database.sql
@@ -9,7 +9,10 @@ CREATE TABLE "koalas"(
 
 INSERT INTO "koalas" (
 	"name", "gender", "age", "ready_to_transfer", "notes")
-VALUES ('Scotty', 'M', '4', 'true', 'Born in Guatemala');
-INSERT INTO "koalas" (
-	"name", "gender", "age", "ready_to_transfer", "notes")
-VALUES ('Jean', 'F', '5', 'true', 'Allergic to lots of lava'), ('Ororo', 'F', '7', 'false', 'Loves listening to Paula Abdul'), ('Logan', 'M', '15', 'false', 'Loves the sauna'), ('Charlie', 'M', '9', 'true', 'Favorite band is Nirvana'), ('Betsy', 'F', '4', 'true', 'Has a pet iguana');
+VALUES 
+('Logan', 'M', '12', 'true', 'Born in Guatemala'),
+('Connor', 'M', '6', 'true', 'Allergic to lots of lava'), 
+('Kendall', 'M', '5', 'false', 'Loves listening to Paula Abdul'), 
+('Shiv', 'F', '4', 'false', 'Loves the sauna'), 
+('Roman', 'M', '2', 'true', 'Favorite band is Nirvana'), 
+('Stewie', 'M', '5', 'true', 'Has a pet iguana');
